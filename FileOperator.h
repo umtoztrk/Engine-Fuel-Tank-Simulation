4e0163e91e742f3257ff4e87ac134c723c5e05f0
Umut Öztürk <umtozturk0348@gmail.com>
2024-05-18 15:44:26 +0300

    This file defines the FileOperator class, which serves as the base class for file operations.

---
+++ FileOperator.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+class FileOperator {
+protected:
+    string filePath;
+public:
+    FileOperator(){}
+};
